351073a97f28b41f3bd2a0076979ce66daf07a73
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-29 14:10:37 +0400

    fix: correct typo in ID name

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded',function() {
     const taskInput = document.getElementById('task-input');
     const addTaskBtn = document.getElementById('add-task-btn');
-    const taskList = document.getElementById('taks-list');
+    const taskList = document.getElementById('task-list');
 
     addTaskBtn.addEventListener('click',function() {
         let taskText = taskInput.value.trim();
         let toUpperCaseFirstLetter = taskText.charAt(0).toUpperCase() + taskText.slice(1).toLowerCase();
         taskText = toUpperCaseFirstLetter;
         if (taskText === "") return;
-
+       
         const li = document.createElement('li');
         li.textContent = taskText;
+
+
         taskList.appendChild(li);
     })
 })
